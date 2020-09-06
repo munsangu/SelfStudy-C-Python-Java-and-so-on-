28cd87c71d3f66f13b33caa4b7e734b36a979a12
munsangu <ds-3632@hanmail.net>
2020-09-06 23:55:31 +0900

    selfstudy nadocoind 2- 6

---
+++ NadoCoding/NadoCoding/printfscanf.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void) 
+{	
+	// 정수형 변수에 대한 예제
+	/*int age = 12;
+	printf("%d\n", age);
+	age = 13;
+	printf("%d\n", age);*/
+
+	// 실수형 변수에 대한 예제
+	/*float f = 46.5f;
+	printf("%.2f\n", f);
+	double d = 4.428;
+	printf("%.2lf\n", d);*/
+
+	//const int YEAR = 2000;// 상수
+	//printf("태어난 년도 : %d\n", YEAR);
+	//YEAR = 2001;
+
+	//printf
+	// 연산
+	int add = 3 + 7;		// 10
+	printf("3 + 7 = %d\n", add);
+	printf("%d + %d = %d\n", 3, 7, 3 + 7);
+	printf("%d * %d = %d\n", 30, 79, 30 * 79);
+
+	return 0;
+}
